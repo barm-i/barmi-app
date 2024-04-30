3f187f9047d85c51a9a391ca9e61f23a907c9062
codernineteen <wjsdPcks34@gmail.com>
2024-04-30 19:34:34 +0900

    added user model

---
+++ server/src/db/models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", UserSchema);
