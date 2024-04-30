b7c5114cb59c7d9928fd34b5cb67f2b609ca7b7e
codernineteen <wjsdPcks34@gmail.com>
2024-04-30 19:35:49 +0900

    add new route for login

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,13 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
+// controllers
+import { loginUser } from "../controllers/loginHandler.js";
 
 // global router for api routes
 const router = Router();
 
 export function apiRouter() {
-    // set routes
-    router.get('/', (req, res) => {
-        res.json({ message: 'api route' });
-    });
+  // set routes
+  router.get("/", (req, res) => {
+    res.json({ message: "api route" });
+  });
+  router.post("/login", loginUser);
 
-    return router;
+  return router;
 }
