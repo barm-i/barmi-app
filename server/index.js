80daadc2939f8417a07dec7fc81c57860ea7c5f0
codernineteen <wjsdPcks34@gmail.com>
2024-04-30 15:32:13 +0900

    modularize server entry point

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
+// third parties
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
+// internal modules
+import connectMongoDB from './src/db/connect.js';
+// api router
 import { apiRouter } from './src/routes/api.js';
+import { start } from 'repl';
 
-// configuration
-const PORT = 3000;
+// server configuration
+dotenv.config();
 const __DIRNAME = path.resolve();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
 
-
+// server instance
 const app = express();
 
 // set middleware
@@ -19,6 +27,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__DIRNAME, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+async function startServer() {
+    try {
+        await connectMongoDB(DB_URI);
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(`error while starting server : ${error.message}`)
+    }
+} 
+
+startServer();
