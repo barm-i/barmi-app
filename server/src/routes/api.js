b2a5851ae457e3098f56d429324cf7b92671e113
codernineteen <wjsdPcks34@gmail.com>
2024-05-23 19:58:16 +0900

    add https agent

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -92,6 +92,7 @@ export function apiRouter() {
             formData,
             {
               headers: formData.getHeaders(),
+              httpsAgent: httpsAgent,
             }
           );
 
