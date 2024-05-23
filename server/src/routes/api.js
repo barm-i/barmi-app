d834881fa22e45ae24bf201c3ea46fbd366fba37
codernineteen <wjsdPcks34@gmail.com>
2024-05-23 13:49:33 +0900

    update url - env

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
+import axios from "axios";
+import FormData from "form-data";
+import dotenv from "dotenv";
 // controllers
 import { loginUser } from "../controllers/loginHandler.js";
 import { signupUser, storeFontStyle } from "../controllers/signupHandler.js";
-import { uploader, handleFormData } from "../controllers/imageHandler.js";
+import { uploader } from "../controllers/imageHandler.js";
 import { ensureAuthenticated } from "../middleware/auth.js";
 import {
   requestRankRows,
   updateMyPoint,
 } from "../controllers/leaderBoardHandler.js";
 import { clearDocuments } from "../controllers/clearDocumentsAll.js";
+import { User } from "../db/models/user.js";
+
+// config env
+dotenv.config();
 
 // global router for api routes
 const router = Router();
@@ -25,7 +32,79 @@ export function apiRouter() {
   router.post(
     "/upload_image",
     /* image middleware */ uploader.array("image", 2),
-    handleFormData
+    async (req, res) => {
+      const { text, flag } = req.body;
+
+      // Create a new FormData instance
+      const formData = new FormData();
+
+      const uniqueName1 = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const uniqueName2 = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+      const FINAL_SERVER_URL = process.env.AI_HTTP_SERVER_URI
+        ? process.env.AI_HTTP_SERVER_URI
+        : process.env.AI_HTTPS_SERVER_URI
+        ? process.env.AI_HTTPS_SERVER_URI
+        : "none";
+      if (FINAL_SERVER_URL === "none") {
+        return res.status(500).json({ message: "AI server not found" });
+      }
+
+      //Append the images and textContent to formData
+      formData.append("font_photo", req.files[0].buffer, {
+        filename: uniqueName1,
+        contentType: req.files[0].mimetype,
+      });
+      formData.append("handwriting_photo", req.files[1].buffer, {
+        filename: uniqueName2,
+        contentType: req.files[1].mimetype,
+      });
+      formData.append("text", text);
+
+      if (flag === "game") {
+        // TODO : 게임 결과 디비 반영
+        try {
+          const response = await axios.post(
+            `${FINAL_SERVER_URL}/game`,
+            formData,
+            {
+              headers: formData.getHeaders(),
+            }
+          );
+
+          console.log("game result", response.data);
+
+          return res.status(200).json({ message: "game result updated" });
+        } catch (error) {
+          console.log("cannot connect to AI server");
+          console.error(error);
+          return res.sendStatus(500);
+        }
+      } else {
+        // 피드백 반환
+        try {
+          const response = await axios.post(
+            `${FINAL_SERVER_URL}/feedback`,
+            formData,
+            {
+              headers: formData.getHeaders(),
+            }
+          );
+
+          if (response.data.feedbacks) {
+            response.data.feedbacks.forEach((feedback) => {
+              console.log(feedback.feedback, feedback.coordinates);
+            });
+          }
+
+          return res.status(200).json(response.data);
+        } catch (error) {
+          console.error(error);
+          console.log("cannot connect to AI server");
+          return res.sendStatus(500);
+        }
+      }
+    }
   );
 
   // User info
