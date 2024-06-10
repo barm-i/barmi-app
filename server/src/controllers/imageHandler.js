283f3e49e42df7facef3c0ea5c698ab582e15253
codernineteen <wjsdPcks34@gmail.com>
2024-06-10 22:58:03 +0900

    rollback upload api

--- server/src/controllers/imageHandler.js
+++ server/src/controllers/imageHandler.js
@@ -1,9 +1,12 @@
 import multer from "multer";
+import path from "path";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import { User } from "../db/models/user.js";
 
 dotenv.config();
+
+const __DIRNAME = path.resolve();
 
 export const uploadImageToCloud = (
   username,
@@ -12,9 +15,25 @@ export const uploadImageToCloud = (
   uniqueName1,
   uniqueName2
 ) => {
+  const dir = path.join(__DIRNAME, "/public/records", username);
+
+  const fileStr1 = buffer1.toString("base64");
   const fileStr2 = buffer2.toString("base64");
 
   // upload to cloudinary
+  const upload1 = new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      `data:image/png;base64,${fileStr1}`,
+      { public_id: `${username}/${uniqueName1}` },
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.url);
+        }
+      }
+    );
+  });
   const upload2 = new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       `data:image/png;base64,${fileStr2}`,
@@ -29,12 +48,15 @@ export const uploadImageToCloud = (
     );
   });
 
-  Promise.all([upload2])
+  Promise.all([upload1, upload2])
     .then(([url1, url2]) => {
+      console.log("url1", url1);
+      const file1 = { filename: uniqueName1, date: new Date(), url: url1 };
       const file2 = { filename: uniqueName2, date: new Date(), url: url2 };
 
       User.findOneAndUpdate({ username: username }).then((user) => {
         if (user) {
+          user.files.push(file1);
           user.files.push(file2);
           user.save();
         }
